42af2b2ca5c08926e0f7aeddd016d2f14e1af8bb
BakarRumah <asawibowo03@gmail.com>
2025-03-20 00:59:32 +0700

    DESIGN ULANG UI

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styleindex.css">
+    <title>LANDING PAGE</title>
+</head>
+<body>
+<!--SAMBUTAN-->
+<nav class="nav-overlay">
+            <ul>
+                <li><a href="index.php">BERANDA</li>
+                <li><a href="login.php">AKTIVASI</a></li>
+                <li><a href="register.php">REGISTRASI</a></li>
+            </ul>
+        </nav>
+    <div class="hero">
+    <div class="overlay">
+
+        <div>
+            <h1>SELAMAT DATANG DI WEBSITE SMK NEGERI 4 KENDAL</h1>
+            <p>SMK NEGERI 4 KENDAL UNGGUL BERKARAKTER</p>
+            <a href="login.php" style="color:white;">Login</a>
+        </div>
+    </div>
+</div>
+</body>
+</html>
